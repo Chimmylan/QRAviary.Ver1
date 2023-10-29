1a8004e9ffe5130b0a70e02f67f9a990e516e0ad
Christian Evangelista <cevangelista041@gmail.com>
2023-10-29 23:09:23 +0800

    forgotpasslayout

---
+++ app/src/main/java/com/example/qraviaryapp/activities/mainactivities/ManageUserActivity.kt
@@ -0,0 +1,14 @@
+package com.example.qraviaryapp.activities.mainactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qraviaryapp.R
+
+class ManageUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_user)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/qraviaryapp/activities/mainactivities/SaveLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qraviaryapp.activities.mainactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qraviaryapp.R
+
+class SaveLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_login)
+    }
+}
