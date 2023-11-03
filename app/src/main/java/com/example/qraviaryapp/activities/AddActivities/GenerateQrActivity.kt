37cab6ef2f813e93dd66434ff0085b6afc07f60a
Christian Evangelista <cevangelista041@gmail.com>
2023-11-03 12:40:10 +0800

    movescan

--- app/src/main/java/com/example/qraviaryapp/activities/AddActivities/GenerateQrActivity.kt
+++ app/src/main/java/com/example/qraviaryapp/activities/AddActivities/GenerateQrActivity.kt
@@ -5,11 +5,21 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
 import com.example.qraviaryapp.R
-    class GenerateQrActivity : AppCompatActivity() {
+import com.google.android.material.button.MaterialButtonToggleGroup
+
+class GenerateQrActivity : AppCompatActivity() {
+        private lateinit var togglebutton: MaterialButtonToggleGroup
+        private lateinit var btnegg: Button
+        private lateinit var btnbird: Button
+        private lateinit var egglayout: LinearLayout
+        private lateinit var birdlayout: LinearLayout
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -34,7 +44,29 @@ import com.example.qraviaryapp.R
         )
         // Check if night mode is enabled
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_white)
+            togglebutton =findViewById(R.id.toggleButtonGroup)
+            egglayout = findViewById(R.id.egglayout)
+            birdlayout = findViewById(R.id.birdlayout)
+            btnegg = findViewById(R.id.btnegg)
 
+
+            togglebutton.addOnButtonCheckedListener { group, checkedId, isChecked ->
+
+                if (isChecked) {
+                    when (checkedId) {
+                        R.id.btnegg -> {
+                            egglayout.visibility = View.VISIBLE
+                            birdlayout.visibility = View.GONE
+
+                        }
+                        R.id.btnbird -> {
+                            birdlayout.visibility = View.VISIBLE
+                            egglayout.visibility = View.GONE
+                        }
+                    }
+                }
+
+            }
     }
 
 
