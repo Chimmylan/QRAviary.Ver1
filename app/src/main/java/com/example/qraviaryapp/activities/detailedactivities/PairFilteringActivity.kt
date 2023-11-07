4c912526511e74127bb2c7c56e4ff45020d97c56
Christian Evangelista <cevangelista041@gmail.com>
2023-11-07 20:39:19 +0800

    mutationa-z

---
+++ app/src/main/java/com/example/qraviaryapp/activities/detailedactivities/PairFilteringActivity.kt
@@ -0,0 +1,50 @@
+package com.example.qraviaryapp.activities.detailedactivities
+
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
+import com.example.qraviaryapp.R
+
+class PairFilteringActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.statusbar)
+        }
+        setContentView(R.layout.activity_pair_filter)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.elevation = 0f
+        supportActionBar?.setBackgroundDrawable(
+            ColorDrawable(
+                ContextCompat.getColor(
+                    this,
+                    R.color.toolbarcolor
+                )
+            )
+        )
+        val abcolortitle = resources.getColor(R.color.appbar)
+        supportActionBar?.title = HtmlCompat.fromHtml(
+            "<font color='$abcolortitle'>Pairs Filter</font>",
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+        // Check if night mode is enabled
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_white)
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            android.R.id.home -> {
+                onBackPressed() // Call this to navigate back to the previous fragment
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
