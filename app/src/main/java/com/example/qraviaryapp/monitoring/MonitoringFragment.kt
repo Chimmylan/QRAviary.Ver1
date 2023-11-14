1ff1acb86d3474f5e7de9f0096ee65d92fc800d9
fed0212 <federickgreg20@gmail.com>
2023-11-14 19:58:19 +0800

    Hardware

--- app/src/main/java/com/example/qraviaryapp/monitoring/MonitoringFragment.kt
+++ app/src/main/java/com/example/qraviaryapp/monitoring/MonitoringFragment.kt
@@ -1,15 +1,30 @@
 package com.example.qraviaryapp.monitoring
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.example.qraviaryapp.R
+import com.example.qraviaryapp.activities.mainactivities.HomeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -17,7 +32,10 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.GenericTypeIndicator
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import java.util.Calendar
+import kotlin.math.round
 import kotlin.math.roundToInt
 
 
@@ -42,6 +60,8 @@ class MonitoringFragment : Fragment() {
     private lateinit var fanTextView2: TextView
     private lateinit var dbase: DatabaseReference
     private lateinit var mAuth: FirebaseAuth
+    private val CHANNEL_ID = "MyNotificationChannel"
+    private var lastTemp = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +69,7 @@ class MonitoringFragment : Fragment() {
 
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -72,40 +93,60 @@ class MonitoringFragment : Fragment() {
         val db = FirebaseDatabase.getInstance().getReference("Users")
             .child("ID: ${currentUserId.toString()}")
 
-        db.addValueEventListener(object: ValueEventListener{
+        db.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val roomDetails = dataSnapshot.child("Aviary")
                     .getValue(object : GenericTypeIndicator<HashMap<String, Any>>() {})
 
                 val temperature = roomDetails?.get("temperature")
                 val humidity = roomDetails?.get("humidity")
-//
-//                val temperatureTextView = view.findViewById<TextView>(R.id.temperature_text_view)
-//                val humidityTextView = view.findViewById<TextView>(R.id.humidity_text_view)
+
+                val temperatureTextView = view.findViewById<TextView>(R.id.temperature_text_view)
+                val humidityTextView = view.findViewById<TextView>(R.id.humidity_text_view)
 
                 if (temperature is Number) {
                     // Check if it's a Long or Double and then convert to Double
-                    val temperatureDouble = if (temperature is Long) temperature.toDouble() else temperature.toDouble()
+                    val temperatureDouble =
+                        if (temperature is Long) temperature.toDouble() else temperature.toDouble()
+//                    temperatureTextView.text = "Temperature: ${temperatureDouble.roundToInt()} °C"
 
                     var maxTemperature: Float = 30.0F
 
-                    if (temperatureDouble >= maxTemperature){
+                    if (temperatureDouble >= maxTemperature) {
                         fanTextView = view.findViewById(R.id.fan_text_view)
                         fanTextView2 = view.findViewById(R.id.fan_text_view2)
                         fanTextView.text = "It is too hot!"
                         fanTextView2.text = "Fan is on."
-                    } else if(temperatureDouble < maxTemperature){
+                    } else if (temperatureDouble < maxTemperature) {
                         fanTextView.text = "Normal Temperature."
                         fanTextView2.text = "Fan is off!"
-                    } else{
-
+                    } else {
+                        fanTextView.text = "Sensor is not working!"
+                        fanTextView2.text = "Fan is Unavailable!"
                     }
+
+//                    notification
+                    val roundedTemp = temperatureDouble.roundToInt()
+                    val tempDifference = roundedTemp - lastTemp
+                    val tempThreshold = 0.5
+
+                    if(temperatureDouble != null && tempDifference >= tempThreshold){
+
+                        sendTempNotification("High", temperatureDouble)
+                        lastTemp = temperatureDouble
+                    }
+
+//                    if(temperatureDouble != null && temperatureDouble > maxTemperature){
+//                        sendTempNotification("High", temperatureDouble)
+//                    }
 
                 }
 
+
                 if (humidity is Number) {
                     // Check if it's a Long or Double and then convert to Double
-                    val humidityDouble = if (humidity is Long) humidity.toDouble() else humidity.toDouble()
+                    val humidityDouble =
+                        if (humidity is Long) humidity.toDouble() else humidity.toDouble()
 //                    humidityTextView.text = "Humidity: ${humidityDouble.roundToInt()} %"
                 }
 
@@ -132,21 +173,26 @@ class MonitoringFragment : Fragment() {
                 val temperatureIncubator = incubatorDetails?.get("temperature")
                 val humidityIncubator = incubatorDetails?.get("humidity")
 
-                val temperatureIncubatorTextView = view.findViewById<TextView>(R.id.temperature_incubator_text_view)
-                val humidityIncubatorTextView = view.findViewById<TextView>(R.id.humidity_incubator_text_view)
+                val temperatureIncubatorTextView =
+                    view.findViewById<TextView>(R.id.temperature_incubator_text_view)
+                val humidityIncubatorTextView =
+                    view.findViewById<TextView>(R.id.humidity_incubator_text_view)
                 val incubatorTextView = view.findViewById<TextView>(R.id.incubatorTextView)
 
                 if (temperatureIncubator is Number) {
                     // Check if it's a Long or Double and then convert to Double
                     incubatorTextView.text = "Incubator is Working"
 
-                    val temperatureIncubatorDouble = if (temperatureIncubator is Long) temperatureIncubator.toDouble() else temperatureIncubator.toDouble()
-                    temperatureIncubatorTextView.text = "Temperature: $temperatureIncubatorDouble °C"
+                    val temperatureIncubatorDouble =
+                        if (temperatureIncubator is Long) temperatureIncubator.toDouble() else temperatureIncubator.toDouble()
+                    temperatureIncubatorTextView.text =
+                        "Temperature: $temperatureIncubatorDouble °C"
                 }
 
                 if (humidityIncubator is Number) {
                     // Check if it's a Long or Double and then convert to Double
-                    val humidityIncubatorDouble = if (humidityIncubator is Long) humidityIncubator.toDouble() else humidityIncubator.toDouble()
+                    val humidityIncubatorDouble =
+                        if (humidityIncubator is Long) humidityIncubator.toDouble() else humidityIncubator.toDouble()
                     humidityIncubatorTextView.text = "Humidity: $humidityIncubatorDouble %"
                 }
 
@@ -171,8 +217,68 @@ class MonitoringFragment : Fragment() {
 //        }
 
 
+        createNotificationChannel()
+
         return view
     }
 
+    private fun sendTempNotification(alertType: String, temperatureDouble: Double) {
+        val tempHot = 30.0
+        val tempNormal = 29.0
 
-}
+        when {
+            temperatureDouble >= tempHot -> {
+                sendNotification("High", temperatureDouble, "It is too hot! Fan is on.")
+            }
+            temperatureDouble < tempNormal -> {
+                sendNotification("Normal", temperatureDouble, "Normal Temperature, Fan is off.")
+            }
+        }
+    }
+
+    private fun sendNotification(alertType: String, temperatureDouble: Double, notificationMessage: String) {
+        val intent = Intent(requireContext(), MonitoringFragment::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent = PendingIntent.getActivity(requireContext(), 0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification_icon)
+            .setContentTitle("Temperature Alert")
+            .setContentText("Temperature is $temperatureDouble°C. $notificationMessage")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        // Show the notification
+        val notificationManager = NotificationManagerCompat.from(requireContext())
+        val notificationId = System.currentTimeMillis().toInt()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(notificationId, builder.build())
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "My Notification Channel"
+            val descriptionText = "Channel for my notifications"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+//            channel.description = description
+//            channel.enableLights(true)
+//            channel.lightColor = Color.BLUE
+//            channel.enableVibration(true)
+//            val notificationManager = requireContext().getSystemService(NotificationManager::class.java)
+//            notificationManager.createNotificationChannel(channel)
+            val notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+             }
+        }
+    }
