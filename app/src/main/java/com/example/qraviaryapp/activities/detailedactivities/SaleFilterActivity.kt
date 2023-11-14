06477913394c45618062f10614d0d664a75a8e10
Kevinroy <77309706+Slimeue@users.noreply.github.com>
2023-11-14 23:20:16 +0800

    Update: Bird Filter

--- app/src/main/java/com/example/qraviaryapp/activities/detailedactivities/SaleFilterActivity.kt
+++ app/src/main/java/com/example/qraviaryapp/activities/detailedactivities/SaleFilterActivity.kt
@@ -1,15 +1,36 @@
 package com.example.qraviaryapp.activities.detailedactivities
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
 import com.example.qraviaryapp.R
+import com.google.android.material.button.MaterialButton
 
 class SaleFilterActivity : AppCompatActivity() {
+
+
+    private lateinit var fromDatePickerDialog: DatePickerDialog
+    private lateinit var toatePickerDialog: DatePickerDialog
+
+    private lateinit var fromBtn: MaterialButton
+    private lateinit var toBtn: MaterialButton
+
+
+    private lateinit var buyer: EditText
+    private lateinit var maleCb: CheckBox
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -33,14 +54,28 @@ class SaleFilterActivity : AppCompatActivity() {
         )
         // Check if night mode is enabled
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_white)
+
+
+        sharedPreferences = getSharedPreferences("SalesFilter", Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+
+
     }
 
 
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_filters, menu)
+        return true
+    }
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+
+            R.id.ic_done ->{
+
+                true
+            }
 
             android.R.id.home -> {
                 onBackPressed() // Call this to navigate back to the previous fragment
